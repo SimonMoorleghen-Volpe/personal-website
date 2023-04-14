adbecea2f2c2f686f0d543c1d890a2bddff7c3a5
Simon Moorleghen-Volpe <scvolpe@yahoo.com>
2023-04-14 16:23:48 -0400

    removed alert from radio check function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var current_id = "about_content";
 
 function radio_tab_checked(radio_value) {
-    alert(radio_value);
     document.getElementById(radio_value).style.display = "initial"; 
     document.getElementById(current_id).style.display = "none";
     current_id = radio_value;
