4b4870d5d5acff2280eaa28897f3b2126df5210e
Simon Moorleghen-Volpe <scvolpe@yahoo.com>
2023-04-14 16:19:57 -0400

    renamed main, working on projects page

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var current_id = "about_content";
 
-function radio_checked(radio_value) {
+function radio_tab_checked(radio_value) {
+    alert(radio_value);
     document.getElementById(radio_value).style.display = "initial"; 
     document.getElementById(current_id).style.display = "none";
     current_id = radio_value;
+}
+
+function none(){
+    return;
 }
