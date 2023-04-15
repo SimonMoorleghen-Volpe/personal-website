23b8b65147062fff3367baeaa2b5bc9ec3b63632
Simon Moorleghen-Volpe <scvolpe@yahoo.com>
2023-04-15 12:19:04 -0400

    more workable formatting and working on past projects tab

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var current_id = "about_content";
+var current_proj = "joint_game";
 
 function radio_tab_checked(radio_value) {
-    document.getElementById(radio_value).style.display = "initial"; 
     document.getElementById(current_id).style.display = "none";
+    document.getElementById(radio_value).style.display = "initial"; 
     current_id = radio_value;
 }
 
-function none(){
-    return;
+function radio_project_checked(radio_value){
+    document.getElementById(current_proj).style.display = "none";
+    document.getElementById(radio_value).style.display = "flex";
+    current_proj = radio_value;
 }
